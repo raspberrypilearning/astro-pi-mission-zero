b76ab47c582dcc11644f80a03137de859e7a7c68
ninaszymor <33628387+ninaszymor@users.noreply.github.com>
2021-07-26 21:43:53 +0100

    New translations bluebird.py (Swedish)

--- sv-SE/solutions/bluebird.py
+++ sv-SE/solutions/bluebird.py
@@ -1,14 +1,14 @@
 från sense_hat importera SenseHat
 sense = SenseHat()
 sense.set_rotation(270)
-w = (255, 255, 255) # white
-x = (0, 0, 0) # black
-g = (0,255,0) # green
+w = (255, 255, 255) # vit
+x = (0, 0, 0) # svart
+g = (0,255,0) # grön
 s = (180,180,180) # silver
-r = (255,0,0) # red
+r = (255,0,0) # röd
 c = (66, 220, 240) # cyan
 o = (180,100,0) # orange
-b = (0, 0,255) # blue
+b = (0, 0,255) # blå
 
 picture = [
     c, c, c, c, c, c, c, c,
