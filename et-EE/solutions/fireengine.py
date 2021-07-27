bd5e4199ab8d5623a34af33e14491525b03faccd
ninaszymor <33628387+ninaszymor@users.noreply.github.com>
2021-07-27 20:42:59 +0100

    New translations fireengine.py (Estonian)

--- et-EE/solutions/fireengine.py
+++ et-EE/solutions/fireengine.py
@@ -1,16 +1,16 @@
 from sense_hat import SenseHat
 sense = SenseHat()
 sense.set_rotation(270)
-w = (255, 255, 255) # white
-x = (0, 0, 0) # black
-g = (0,255,0) # green
-s = (180,180,180) # silver
-r = (255,0,0) # red
-c = (66, 220, 240) # cyan
-o = (180,100,0) # orange
-b = (0, 0,255) # blue
+w = (255, 255, 255) # valge
+x = (0, 0, 0) # must
+g = (0,255,0) # roheline
+s = (180,180,180) # hõbedane
+r = (255,0,0) # punane
+c = (66, 220, 240) # tsüaan
+o = (180,100,0) # oranž
+b = (0, 0,255) # sinine
 
-picture = [
+pilt = [
     c, c, c, c, c, c, c, c,
     c, c, c, c, c, c, c, c,
     c, c, c, c, c, c, b, c,
@@ -21,5 +21,5 @@ picture = [
     g, g, g, g, g, g, g, g
     ]
     
-sense.set_pixels(picture)
+sense.set_pixels(pilt)
 
