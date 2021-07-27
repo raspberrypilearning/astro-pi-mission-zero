c0bbf6edd547c8f9c0b956854a8934c0359f4a20
ninaszymor <33628387+ninaszymor@users.noreply.github.com>
2021-07-27 08:50:19 +0100

    New translations fu_pixels.py (Dutch)

--- nl-NL/solutions/fu_pixels.py
+++ nl-NL/solutions/fu_pixels.py
@@ -1,13 +1,13 @@
 from sense_hat import SenseHat
 sense = SenseHat()
 sense.set_rotation(270)
-w = (255, 255, 255)
-b = (0, 0, 0)
+w = (255,255,255)
+b = (0,0,0)
 g = (50,50,50)
 s = (180,180,180)
 r = (255,0,0)
 
-picture = [
+afbeelding = [
     g, b, b, b, b, b, b, g,
     b, g, g, g, g, g, g, b,
     b, g, b, b, g, w, g, s,
