75ef6371443fe0f0693b12f6b023b21208c41608
ninaszymor <33628387+ninaszymor@users.noreply.github.com>
2021-07-28 09:14:52 +0100

    New translations bluebird.py (Flemish)

--- vls-BE/solutions/bluebird.py
+++ vls-BE/solutions/bluebird.py
@@ -1,16 +1,16 @@
-from sense_hat import SenseHat
+importeer sense_hat van SenseHat
 sense = SenseHat()
 sense.set_rotation(270)
 w = (255, 255, 255) # white
 x = (0, 0, 0) # black
 g = (0,255,0) # green
-s = (180,180,180) # silver
-r = (255,0,0) # red
+s = (180, 180, 180) # silver
+r = (255,0,0,) # red
 c = (66, 220, 240) # cyan
 o = (180,100,0) # orange
 b = (0, 0,255) # blue
 
-picture = [
+foto = [
     c, c, c, c, c, c, c, c,
     c, c, b, b, b, c, c, c,
     c, b, b, x, b, b, c, c,
