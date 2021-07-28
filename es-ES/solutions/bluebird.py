a0f629ab81bd09f60d4dd93efb1c9a7371c419f4
ninaszymor <33628387+ninaszymor@users.noreply.github.com>
2021-07-28 12:23:18 +0100

    New translations bluebird.py (Spanish)

--- es-ES/solutions/bluebird.py
+++ es-ES/solutions/bluebird.py
@@ -1,16 +1,16 @@
 from sense_hat import SenseHat
 sensor = SenseHat()
-sense.set_rotation(270)
-w = (255, 255, 255) # white
-x = (0, 0, 0) # black
-g = (0,255,0) # green
-s = (180,180,180) # silver
-r = (255,0,0) # red
-c = (66, 220, 240) # cyan
-o = (180,100,0) # orange
-b = (0, 0,255) # blue
+sense.set_rotation (270)
+w = (255, 255, 255) # blanco
+x = (0, 0, 0) # negro
+g = (0,255,0) # verde
+s = (180,180,180) # plateado
+r = (255,0,0) # rojo
+c = (66, 220, 240) # cian
+o = (180,100,0) # naranja
+b = (0, 0,255) # azul
 
-picture = [
+imagen = [
     c, c, c, c, c, c, c, c,
     c, c, b, b, b, c, c, c,
     c, b, b, x, b, b, c, c,
