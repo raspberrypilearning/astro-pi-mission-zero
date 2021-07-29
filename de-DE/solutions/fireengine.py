836f5883e3a98dae2c0db64a587529a6f5105e56
ninaszymor <33628387+ninaszymor@users.noreply.github.com>
2021-07-29 10:35:36 +0100

    New translations fireengine.py (German)

--- de-DE/solutions/fireengine.py
+++ de-DE/solutions/fireengine.py
@@ -1,16 +1,16 @@
 from sense_hat import SenseHat
 sense = SenseHat()
 sense.set_rotation(270)
-w = (255, 255, 255) # white
-x = (0, 0, 0) # black
-g = (0,255,0) # green
-s = (180,180,180) # silver
-r = (255,0,0) # red
-c = (66, 220, 240) # cyan
-o = (180,100,0) # orange
-b = (0, 0,255) # blue
+w = (255, 255, 255) # Weiss
+x = (0, 0, 0) # Schwarz
+g = (0,255,0) # Grün
+s = (180,180,180) # Silber
+r = (255,0,0) # Rot
+c = (66, 220, 240) # Cyan
+o = (180,100,0) # Orange
+b = (0, 0,255) # Blau
 
-picture = [
+bild = [
     c, c, c, c, c, c, c, c,
     c, c, c, c, c, c, c, c,
     c, c, c, c, c, c, b, c,
