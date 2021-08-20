1c645127f6b844eb734325662c2bf82c2bc146ac
ninaszymor <33628387+ninaszymor@users.noreply.github.com>
2021-08-20 11:28:35 +0100

    New translations fu_pixels.py (Polish)

--- pl-PL/solutions/fu_pixels.py
+++ pl-PL/solutions/fu_pixels.py
@@ -5,9 +5,9 @@ w = (255, 255, 255)
 b = (0, 0, 0)
 g = (50,50,50)
 s = (180,180,180)
-r = (255,0,0)
+red = (255,0,0)
 
-picture = [
+obrazek = [
     g, b, b, b, b, b, b, g,
     b, g, g, g, g, g, g, b,
     b, g, b, b, g, w, g, s,
@@ -18,4 +18,4 @@ picture = [
     g, b, b, b, b, b, b, g
     ]
     
-sense.set_pixels(picture)
+sense.set_pixels(obrazek)
