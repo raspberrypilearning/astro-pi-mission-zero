da1aa2e50c7c1cec4c6d3bb0d4779be0edb71463
ninaszymor <33628387+ninaszymor@users.noreply.github.com>
2021-08-26 13:02:09 +0100

    New translations bluebird.py (Dutch)

--- nl-NL/solutions/bluebird.py
+++ nl-NL/solutions/bluebird.py
@@ -1,16 +1,16 @@
 from sense_hat import SenseHat
 sense = SenseHat()
 sense.set_rotation(270)
-w = (255,255,255) # wit
-x = (0,0,0) # zwart
-g = (0, 255,0) # groen
-s = (180,180,180) # zilver
-r = (255,0,0) # rood
-c = (66,220,240) # cyaan
-o = (180,100,0) # oranje
-b = (0,0,255) # blauw
+w = (255, 255, 255) # white
+x = (0, 0, 0) # black
+g = (0,255,0) # green
+s = (180,180,180) # silver
+r = (255,0,0) # red
+c = (66, 220, 240) # cyan
+o = (180,100,0) # orange
+b = (0, 0,255) # blue
 
-afbeelding = [
+picture = [
     c, c, c, c, c, c, c, c,
     c, c, b, b, b, c, c, c,
     c, b, b, x, b, b, c, c,
@@ -21,4 +21,4 @@ afbeelding = [
     b, b, b, b, b, c, c, c
     ]
     
-sense.set_pixels(afbeelding)
+sense.set_pixels(picture)
