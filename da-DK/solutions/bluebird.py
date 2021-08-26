45940a6b1d91ed2110bc611985ca32fd03a6f63d
ninaszymor <33628387+ninaszymor@users.noreply.github.com>
2021-08-26 13:02:05 +0100

    New translations bluebird.py (Danish)

--- da-DK/solutions/bluebird.py
+++ da-DK/solutions/bluebird.py
@@ -1,19 +1,19 @@
-fra sense_hat importer SenseHat
-sense = SenseHat ()
+from sense_hat import SenseHat
+sense = SenseHat()
 sense.set_rotation(270)
-w = (255, 255, 255) # hvid
-x = (0, 0, 0) # sort
-g = (0,255,0) # grøn
-s = (180,180,180) # sølv
-r = (255,0,0) # rød
+w = (255, 255, 255) # white
+x = (0, 0, 0) # black
+g = (0,255,0) # green
+s = (180,180,180) # silver
+r = (255,0,0) # red
 c = (66, 220, 240) # cyan
 o = (180,100,0) # orange
-b = (0, 0,255) # blå
+b = (0, 0,255) # blue
 
-billede = [
+picture = [
     c, c, c, c, c, c, c, c,
     c, c, b, b, b, c, c, c,
-    c, b, b, x, b, b, c, c, c,
+    c, b, b, x, b, b, c, c,
     c, b, b, b, b, o, o, c,
     c, b, b, b, b, b, c, c,
     b, b, b, b, b, c, c, c,
