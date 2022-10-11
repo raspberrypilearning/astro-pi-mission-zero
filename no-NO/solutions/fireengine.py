116707f59e2c55922010b3163b4653bec54c591d
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2022-10-11 13:51:01 +0100

    New translations fireengine.py (Norwegian)

--- no-NO/solutions/fireengine.py
+++ no-NO/solutions/fireengine.py
@@ -1,14 +1,14 @@
 from sense_hat import SenseHat
 sense = SenseHat()
 sense.set_rotation(270)
-w = (255, 255, 255) # white
-x = (0, 0, 0) # black
-g = (0,255,0) # green
-s = (180,180,180) # silver
-r = (255,0,0) # red
+w = (255, 255, 255) # hvit
+x = (0, 0, 0) # svart
+g = (0,255,0) # grønn
+s = (180,180,180) # sølv
+r = (255,0,0) # rød
 c = (66, 220, 240) # cyan
-o = (180,100,0) # orange
-b = (0, 0,255) # blue
+o = (180,100,0) # oransje
+b = (0, 0,255) # blå
 
 picture = [
     c, c, c, c, c, c, c, c,
