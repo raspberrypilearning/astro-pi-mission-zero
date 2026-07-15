b9d8acfde55baa55be351476c8a65aae037ca8e5
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2026-07-15 12:46:40 +0100

    New translations bluebird.py (Flemish)
    [ci skip]

--- vls-BE/solutions/bluebird.py
+++ vls-BE/solutions/bluebird.py
@@ -4,8 +4,8 @@ sense.set_rotation(270)
 w = (255, 255, 255) # white
 x = (0, 0, 0) # black
 g = (0,255,0) # green
-s = (180, 180, 180) # silver
-r = (255,0,0,) # red
+s = (180,180,180) # silver
+r = (255,0,0) # red
 c = (66, 220, 240) # cyan
 o = (180,100,0) # orange
 b = (0, 0,255) # blue
